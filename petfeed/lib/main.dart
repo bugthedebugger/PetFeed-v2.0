7086c2967110d31ca2e67e7d932bdbc06d56eb65
bprayush <bprayush@yahoo.com>
2019-05-01 14:30:01 +0545

    Initializing mobile app

---
+++ petfeed/lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(PetFeed());
+}
+
+class PetFeed extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      initialRoute: '/',
+    );
+  }
+}
