546b6e9323a99484666882213942f2633c4259cb
bprayush <bprayush@yahoo.com>
2019-05-01 14:49:44 +0545

    Code cleanup and commentings

--- pi/main/petfeed_pi.py
+++ pi/main/petfeed_pi.py
@@ -3,28 +3,34 @@ import RPi.GPIO as GPIO
 # SET THE PIN LAYOUT TO BCM LAYOUT
 GPIO.setmode(GPIO.BCM)
 
-# MOTOR'S TERMINAL 1
-left_pin = 14
-# MOTOR'S TERMINAL 2
-right_pin = 15
-
 
 class MotorControl:
     def __init__(self, left_pin=14, right_pin=15):
+        # ASSIGN THE PIN NUMBERS, 14 AND 15 IF NONE PASSED
         self.left_pin = left_pin
         self.right_pin = right_pin
-
+        # INITIALIZE LEFT AND RIGHT PINS OF THE MOTOR AS OUTPUT
         GPIO.setup([self.left_pin, self.right_pin], GPIO.OUT)
-
+        # INITIALIZE LEFT AND RIGHT PINS AS PWM OUTPUT AT 50hz
         self.left_pwm = GPIO.PWM(self.left_pin, 50)
         self.right_pwm = GPIO.PWM(self.right_pin, 50)
+        # INITIALIZE THE PINS AT 0hz i.e. BOTH PINS SET AS OFF
         self.left_pwm.start(0)
         self.right_pwm.start(0)
 
+    # FUNCTION THAT RUNS THE MOTOR
     def start(self):
+        # CHANGING DUTY CYCLE TO 100 MEANS SUPPLY FULL 5v TO THAT PIN
+        # WHILE 0 MEANS NONE
         self.left_pwm.ChangeDutyCycle(0)
         self.right_pwm.ChangeDutyCycle(100)
 
     def stop(self):
         self.left_pwm.ChangeDutyCycle(0)
         self.right_pwm.ChangeDutyCycle(0)
+
+# FUNCTION TO CLEAN UP USED RPi CHANNELS
+
+
+def end():
+    GPIO.cleanup()
