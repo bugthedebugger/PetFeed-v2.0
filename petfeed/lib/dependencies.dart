fe86b0b97cfeede874b0b2a041b3eb2753507ac5
bprayush <bprayush@yahoo.com>
2019-05-02 19:21:21 +0545

    cleaning unused imports

--- petfeed/lib/dependencies.dart
+++ petfeed/lib/dependencies.dart
@@ -1,9 +1,7 @@
 import 'package:kiwi/kiwi.dart';
 import 'dart:async';
 import 'package:http/http.dart' as http;
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 Future initKiwi() async {
   Container().registerInstance(http.Client);
-  // Container().registerSingleton((c) => ScreenUtil.instance);
 }
--- petfeed/lib/src/assets/font_size.dart
+++ petfeed/lib/src/assets/font_size.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/widgets.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'screen_size.dart';
 
