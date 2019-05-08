eb63a0d7c04d6635954c7af270e09fe234225453
bprayush <bprayush@yahoo.com>
2019-05-08 12:46:50 +0545

    uncommenting to test with pi

--- pi/main/main.py
+++ pi/main/main.py
@@ -2,7 +2,7 @@
 
 from server import FlaskServer
 from models.model import Model
-# from hw_controllers.motor_controller import MotorController
+from hw_controllers.motor_controller import MotorController
 from models.dbcontroller import DBController
 
 db = DBController()
