e6a47a1df28190182d96652b6162ba8716e37a3c
bprayush <bprayush@yahoo.com>
2019-05-11 09:44:16 +0545

    Removed db seeders

--- PetFeed-Server/database/seeds/DatabaseSeeder.php
+++ PetFeed-Server/database/seeds/DatabaseSeeder.php
@@ -13,6 +13,6 @@ class DatabaseSeeder extends Seeder
     {
         // $this->call(UsersTableSeeder::class);
         // $this->call(AdminSeeder::class);
-        $this->call(type_seeder::class);
+        // $this->call(type_seeder::class);
     }
 }
