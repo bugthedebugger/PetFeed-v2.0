c09d53e5abb32006fb36222648b64a74a0b690b1
bprayush <bprayush@yahoo.com>
2019-05-13 11:28:02 +0545

    Adding error status code to flask server

--- pi/main/server.py
+++ pi/main/server.py
@@ -4,6 +4,7 @@ import os
 from flask import Flask
 from flask import jsonify
 from flask import request
+from flask import status
 from models.model import Model
 from hw_controllers.motor_controller import MotorController
 from models.device import Device
@@ -114,7 +115,7 @@ class FlaskServer:
                     'connection': 'local',
                     'status': 'error',
                     'id': None
-                })
+                }), status.HTTP_401_UNAUTHORIZED
 
     @app.route('/wifisetup', methods=['POST'])
     def wifiSetup():
