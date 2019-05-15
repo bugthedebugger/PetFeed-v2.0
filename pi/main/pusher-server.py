7d4e8e52679a6408d449d4903ebbf33453eafc01
bprayush <bprayush@yahoo.com>
2019-05-15 07:34:11 +0545

    Fixed the pusher server

--- pi/main/pusher-server.py
+++ pi/main/pusher-server.py
@@ -1,19 +1,30 @@
+#! /usr/bin/python3
+
 import os
-import pusher
+# from pusher import Pusher as PusherEvent
+import time
+
+import pysher as PusherClient
 from models.dbcontroller import DBController
 from models.device import Device
 import ast
 
+channel = 'petfeed123'
+
+
+def test(data):
+    print(data)
+
 
 class PusherContainer:
-    channel = 'presence-pet'
+    pusherClient = None
 
     @staticmethod
     def password_reset(data):
         data = ast.literal_eval(data)
         if data['channel'] == channel:
             db = DBContoller()
-            device=Device()
+            device = Device()
             result = db.selectAll(device)
             rpassword = result[0]
             if rpassword.password == data['oldpassword']:
@@ -42,42 +53,42 @@ class PusherContainer:
         data = ast.literal_eval(data)
         amount = data['amount']
 
-    @staticmethod
-    def restart(self):
-        self.os.system("sudo reboot")
+        print(amount)
 
     @staticmethod
-    def shutdown(self):
-        self.os.system("sudo poweroff")
+    def restart(data):
+        os.system("sudo reboot")
 
     @staticmethod
-    def connect_handler(data):
-        pusher_client = pusher.Pusher(
-            app_id='778757',
-            key='cad1fdb3d70c39d4d373',
-            secret='bd983889d79748783bef',
-            cluster='ap2',
-            ssl=True
-        )
+    def shutdown(data):
+        os.system("sudo poweroff")
+
+    @staticmethod
+    def test(data):
+        print(data)
+
+    def connect_handler(self, data):
+        print('here')
+        petfeed_channel = self.pusherClient.subscribe(channel)
+        petfeed_channel.bind('test', self.test)
+        petfeed_channel.bind('petfeed-restart', self.restart)
+        petfeed_channel.bind('petfeed-reset-password', self.password_reset)
+        petfeed_channel.bind('petfeed-shutdown', self.shutdown)
+        petfeed_channel.bind('petfeed-configure', self.configure)
+        petfeed_channel.bind('petfeed-treat', self.treat)
+
+    def connect(self):
+        self.pusherClient = PusherClient.Pusher(key='cb1bc25895d91dec8e2d', secret='fb24a8ddf38ffd23b205',
+                                                cluster='ap2')
+        self.pusherClient.connection.bind('pusher:connection_established', self.connect_handler)
+        self.pusherClient.connect()
+
+        while True:
+            time.sleep(1)
 
 
-
-        petfeed_channel = pusher_client.subscribe(channel)
-        petfeed_channel.bind('petfeed-restart', restart)
-        petfeed_channel.bind('petfeed-reset-password', password_reset)
-        petfeed_channel.bind('petfeed-shutdown', shutdown)
-        petfeed_channel.bind('petfeed-configure', configure)
-        petfeed_channel.bind('petfeed-treat', treat)
-
-        pusher_client = PusherClient.Pusher(key='cad1fdb3d70c39d4d373', secret='bd983889d79748783bef')
-
-        pusher_client.connection.bind('pusher:connection_established', connect_handler)
-
-        pusher_client.connect()
-
-
-if __name__=='__main__':
-    PusherContainer.connect_handler()
+pusherContainer = PusherContainer()
+pusherContainer.connect()
 
 '''
 def connect_handler(data):
@@ -88,4 +99,3 @@ def connect_handler(data):
     petfeed_channel.bind('petfeed-configure',PusherContainer.configure())
     petfeed_channel.bind('petfeed-treat',PusherContainer.treat())
 '''
-
