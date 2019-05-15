3cc0f5795b721049c0669ca6edae73c86872664d
bprayush <bprayush@yahoo.com>
2019-05-15 13:34:44 +0545

    Added pusher to the mein

--- pi/main/main.py
+++ pi/main/main.py
@@ -3,6 +3,10 @@
 from server import FlaskServer
 from models.model import Model
 from hw_controllers.motor_controller import MotorController
+from pusher_server import PusherContainer
+
 
 flaskServer = FlaskServer()
 flaskServer.start()
+pusherContainer = PusherContainer()
+pusherContainer.connect()
