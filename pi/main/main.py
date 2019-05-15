8bb295ba31a2cdec23b890f3cd85280d9d46249b
bprayush <bprayush@yahoo.com>
2019-05-15 13:55:07 +0545

    Added threading to pi-server

--- pi/main/main.py
+++ pi/main/main.py
@@ -4,9 +4,23 @@ from server import FlaskServer
 from models.model import Model
 from hw_controllers.motor_controller import MotorController
 from pusher_server import PusherContainer
+from threading import Thread
+import signal
 
 
-flaskServer = FlaskServer()
-flaskServer.start()
-pusherContainer = PusherContainer()
-pusherContainer.connect()
+def flask_server():
+    flaskServer = FlaskServer()
+    flaskServer.start()
+    
+def pusher_server():
+    pusherContainer = PusherContainer()
+    pusherContainer.connect()
+
+
+if __name__ == '__main__':
+    flask_thread = Thread(target=flask_server)
+    pusher_thread = Thread(target=pusher_server)
+
+    flask_thread.start()
+    pusher_thread.start()
+
