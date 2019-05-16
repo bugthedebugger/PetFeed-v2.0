006d16ee226c88e651c9cae68447a73aff76649f
UPD619 <upadhyay_saurav@yahoo.com>
2019-05-16 12:22:08 +0545

    added cat calculator

--- foodcalc.py
+++ foodcalc.py
@@ -1,11 +1,19 @@
-class dog_food:
-	def __init__(self, weight):
-		self.weight=weight
+class pet_food:
+	def __init__(self, pet_type,weight):
+		self.weight=int(weight)
+		self.pet_type=str(pet_type).lower()
 
-	def calculator(self):
-		print("amount of food in gram for your dog with weight %skg is %1.2f" %(self.weight, 38.94*(self.weight**0.75)))
-		
+	def dog_calculator(self):
+		if self.pet_type=='dog':
+			print("amount of food in gram for your dog with weight %skg is %1.2f" %(self.weight, 38.94*(self.weight**0.75)))
 
+		else:
+			print('incompatible pet')
 
+	def cat_calculator(self):
+		if self.pet_type=='cat':
+			print("amount of food in gram for your cat with weight %skg is %1.2f" %(self.weight,self.weight*8.6312))
 
+		else:
+		  print('incompatible pet')
 
