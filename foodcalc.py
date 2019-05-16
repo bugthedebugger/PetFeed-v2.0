87a7dec2c087f5b614c751b8147d62696708dc9f
UPD619 <upadhyay_saurav@yahoo.com>
2019-05-16 11:42:26 +0545

    added food calculator

---
+++ foodcalc.py
@@ -0,0 +1,11 @@
+class dog_food:
+	def __init__(self, weight):
+		self.weight=weight
+
+	def calculator(self):
+		print("amount of food in gram for your dog with weight %skg is %1.2f" %(self.weight, 38.94*(self.weight**0.75)))
+		
+
+
+
+
