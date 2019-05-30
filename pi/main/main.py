325a0fa22d6ebea44b0e20a4603c5d0ab8126ec1
bprayush <bprayush@yahoo.com>
2019-05-30 17:40:22 +0545

    Adding debug print

--- pi/main/main.py
+++ pi/main/main.py
@@ -54,7 +54,9 @@ def scheduled_feeding():
         for result in results:
             schedule = Schedule()
             schedule.from_map(result)
+            print(schedule.to_map())
             if schedule.day == today_day and schedule.time == now_time:
+                print('inside first if')
                 if device.type == 'Fish':
                     motorController.fish(duration=schedule.amount)
 
