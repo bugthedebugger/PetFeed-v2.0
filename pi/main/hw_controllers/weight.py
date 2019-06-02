c2abd5510b887fc151cf8d0db0598b40ccf878ef
bprayush <bprayush@yahoo.com>
2019-06-02 12:08:48 +0545

    Testing weight sensor

--- pi/main/hw_controllers/weight.py
+++ pi/main/hw_controllers/weight.py
@@ -9,5 +9,5 @@ class WeightSensor:
         self.hx = HX711(GPIO=GPIO, dout_pin=21, pd_sck_pin=20)
         self.hx.set_scale_ratio(self.ratio)
 
-    def getWeight():
+    def getWeight(self):
         return hx.get_weight_mean(10)
