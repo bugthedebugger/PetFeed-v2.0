0f23e16d9a8d566f4fd1defcb4cf50d798ab3a3a
bprayush <bprayush@yahoo.com>
2019-06-08 21:43:10 +0545

    Adding back to auth

--- PetFeed-Server/routes/api.php
+++ PetFeed-Server/routes/api.php
@@ -39,6 +39,7 @@ Route::group(['middleware' => ['auth:api', 'verified']], function () {
     Route::post('/broadcast/auth', 'Api\BroadcastAuthController@auth');
     // SCHEDULES ROUTES
     Route::post('/schedule/set', 'Api\SchedulesController@createOrUpdate');
+    Route::post('/history/set', 'Api\HistoriesController@createOrUpdate');
 
 });
-Route::post('/history/set', 'Api\HistoriesController@createOrUpdate');
+
